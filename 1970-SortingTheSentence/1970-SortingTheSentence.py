a8d95bd8eb37b7c1586faffeeb8e539dca8aa120
Lokesh <lokeshvj2005@gmail.com>
2026-06-04 22:22:06 +0530

    Create 1970-SortingTheSentence

---
+++ 1970-SortingTheSentence/1970-SortingTheSentence.py
@@ -0,0 +1,6 @@
+# Last updated: 6/4/2026, 10:22:05 PM
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        sentence=s.split(' ')
+        sentence.sort(key=lambda x: x[-1])
+        return ' '.join([i[:-1] for i in sentence])
