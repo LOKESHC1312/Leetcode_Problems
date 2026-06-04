254978c826e6b4b3974746fb4ee560ee95cba219
Lokesh <lokeshvj2005@gmail.com>
2026-06-04 22:22:14 +0530

    Create 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox

---
+++ 1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox/1895-MinimumNumberOfOperationsToMoveAllBallsToEachBox.java
@@ -0,0 +1,17 @@
+// Last updated: 6/4/2026, 10:22:13 PM
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            int moves = 0;
+            for (int j = 0; j < n; j++) {
+                if (boxes.charAt(j) == '1') {
+                    moves += Math.abs(i - j);
+                }
+            }
+            res[i] = moves;
+        }
+        return res;
+    }
+}
