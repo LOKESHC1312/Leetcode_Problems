74f0d68c1087280f6b96a9323879161beb4b9845
Lokesh <lokeshvj2005@gmail.com>
2026-06-04 22:29:31 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.java
@@ -0,0 +1,6 @@
+// Last updated: 6/4/2026, 10:29:30 PM
+class Solution {
+    public boolean isPowerOfThree(int n) {
+    int maxPowerOfThree = 1162261467; return n > 0 && maxPowerOfThree % n == 0;
+    }
+}
