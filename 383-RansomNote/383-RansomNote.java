f7d675a67a78524a4b85bd61973332c1e72e8af0
Lokesh <lokeshvj2005@gmail.com>
2026-06-04 22:29:24 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,19 @@
+// Last updated: 6/4/2026, 10:29:23 PM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq[]=new int[26];
+        for(int i=0;i<ransomNote.length();i++){
+            freq[ransomNote.charAt(i)-'a']++;
+        }
+        for(int i=0;i<magazine.length();i++){
+            freq[magazine.charAt(i)-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]>=1){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
