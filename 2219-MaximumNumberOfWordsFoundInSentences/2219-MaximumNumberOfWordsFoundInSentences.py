b2cf7b0ab235d4d516bbf3d19f9c0d55c23df4e8
Lokesh <lokeshvj2005@gmail.com>
2026-06-04 22:21:18 +0530

    Create 2219-MaximumNumberOfWordsFoundInSentences

---
+++ 2219-MaximumNumberOfWordsFoundInSentences/2219-MaximumNumberOfWordsFoundInSentences.py
@@ -0,0 +1,7 @@
+# Last updated: 6/4/2026, 10:21:05 PM
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        m=0
+        for i in sentences:
+            m=max(m,i.count(" ")+1)
+        return m
